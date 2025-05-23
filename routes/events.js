ef7ee698ce973efe86d0649af20d569cc07fbfe0
廖冠博 <h2itwh2itw@gmail.com>
2025-05-23 17:31:38 +0800

    取得所有活動(approved)

--- routes/events.js
+++ routes/events.js
@@ -6,6 +6,8 @@ const { dataSource } = require('../db/data-source')
 const handleErrorAsync = require('../utils/handleErrorAsync')
 const eventsController = require('../controllers/events')
 
+// 取得所有活動列表
+router.get('/', handleErrorAsync(eventsController.getAllEvents))
 
 // 取得即將登場活動列表
 router.get('/coming-soon', handleErrorAsync(eventsController.getComingEvents))
