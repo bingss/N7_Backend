8430e8ef8e60a761ea9f7eb0519ddac079c19ff8
bingss <thisisbing0315@gmail.com>
2025-05-25 14:27:23 +0800

    temp save

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const eventsRouter = require('./routes/events')
 
 
 const app = express()
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
