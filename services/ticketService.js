8c7fed217e98f53fdd73787ebc51921be64920a0
bingss <thisisbing0315@gmail.com>
2025-06-22 10:04:23 +0800

    verify ticket revise

--- services/ticketService.js
+++ services/ticketService.js
@@ -66,10 +66,10 @@ const verifyTicket = async (ticketInfo, orgEventId, orgUserId) => {
         const nowGMT8 = getNowGMT8Time()
         const start = new Date( ticketWithUserEvent.event_start_at );
         const end = new Date( ticketWithUserEvent.event_end_at );
-        // const dayBeforeStart = new Date(start.getTime() - 24 * 60 * 60 * 1000); 
-        // if(nowGMT8 < dayBeforeStart){
-        //     throw appError(ERROR_STATUS_CODE, `活動開始前1天，票券才開放驗證`)
-        // }
+        const dayBeforeStart = new Date(start.getTime() - 24 * 60 * 60 * 1000); 
+        if(nowGMT8 < dayBeforeStart){
+            throw appError(ERROR_STATUS_CODE, `活動開始前1天，票券才開放驗證`)
+        }
 
         if(end < nowGMT8) {
             throw appError(ERROR_STATUS_CODE, `票券活動已結束`)
